72ca14fc3086e1fd7c875f6deee4d5813c89f518
Ayubxon <ayubxonaxmatxonov@gmail.com>
2025-03-12 17:36:49 +0500

    added code

--- script.js
+++ script.js
@@ -9,10 +9,21 @@ diceImg.style.display ='none'
 
 let currentScore = 0
 let activePlayer = 0
+let score = [0, 0]
+let gameOver = true
 
+
+const switchPlayer = () => {
+  currentScore = 0
+    document.getElementById(`current--${activePlayer}`).textContent = currentScore
+    activePlayer = activePlayer === 0 ? 1 : 0
+    document.querySelector('.player--0').classList.toggle('player--active')
+    document.querySelector('.player--1').classList.toggle('player--active')
+}
 
 btnRoll.addEventListener('click', () => {
-  diceImg.style.display = 'block'
+  if(gameOver) {
+    diceImg.style.display = 'block'
 
   const random = Math.floor(Math.random() * 6 + 1)
   diceImg.src = `dice-${random}.png`
@@ -21,12 +32,37 @@ btnRoll.addEventListener('click', () => {
     currentScore += random
     document.getElementById(`current--${activePlayer}`).textContent = currentScore
   }else {
-    currentScore = 0
-    document.getElementById(`current--${activePlayer}`).textContent = currentScore
-    activePlayer = activePlayer === 0 ? 1 : 0
+    switchPlayer() 
+  }
   }
 })
 
-
-
-
+// hold score
+btnHold.addEventListener('click', () => {
+  if(gameOver) {
+    score[activePlayer] += currentScore
+  document.getElementById(`score--${activePlayer}`).textContent = score[activePlayer]
+  
+  if(score[activePlayer] >= 100) {
+    document.querySelector(`.player--${activePlayer}`).classList.add('player--winner')
+    gameOver = false
+  }else {
+    switchPlayer()
+  }
+  }
+})
+// new game
+btnNew.addEventListener('click', () => {
+  currentScore = 0
+  activePlayer = 0
+  score = [0, 0]
+  gameOver = true
+  document.getElementById('current--0').textContent = 0
+  document.getElementById('current--1').textContent = 0
+  document.getElementById('score--0').textContent = 0
+  document.getElementById('score--1').textContent = 0
+  document.querySelector('.player--0').classList.remove('player--winner')
+  document.querySelector('.player--1').classList.remove('player--winner')
+  document.querySelector('.player--1').classList.remove('player--active')
+  document.querySelector('.player--0').classList.add('player--active')
+})
